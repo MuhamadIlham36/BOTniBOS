216b4fe5eb235bdb209f454819c9fc600bc622c4
MuhamadIlham36 <76248314+MuhamadIlham36@users.noreply.github.com>
2020-12-19 22:50:07 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,7 +3,7 @@ exports.menu = (id, Jenral_¹7, corohelp, tampilTanggal, tampilWaktu, instagram,
   
 HALO *${id.split("@s.whatsapp.net")[0]}* 
 APA KABAR? SEMOGA BAIK-BAIK SAJA!
-Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* 😁
+Sebelum menggunakan bot ini ada baiknya kalian melihat menu *!info* and *!donasi* 😁
 
 
 
